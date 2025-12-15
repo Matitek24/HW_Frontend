d0cf175ed59771ab889ea70e11a15518e0b0e145
Mateusz Skrzypek <k.skrzypek1234@gmail.com>
2025-12-15 09:57:28 +0100

    production_2

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,7 +3,7 @@ import { getStoredToken, removeStoredToken, isTokenValid } from './jwt';
 import router from '../router/index';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
