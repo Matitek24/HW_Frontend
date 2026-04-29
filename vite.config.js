c578aa3ef4a5ca9e9a21901bc497c6018087a400
Mateusz Skrzypek <k.skrzypek1234@gmail.com>
2026-04-29 14:37:00 +0200

    nginx

--- vite.config.js
+++ vite.config.js
@@ -13,4 +13,16 @@ export default defineConfig({
   optimizeDeps: {
     include: ['html2canvas']
   },
+
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
